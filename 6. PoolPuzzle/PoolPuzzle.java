b7deeaf9c645e68622551e49fedc881ddf9a09ed
Andrew <A89176199640@yandex.ru>
2020-02-19 02:49:49 +0400

    добавил новые задачи

---
+++ 6. PoolPuzzle/PoolPuzzle.java
@@ -0,0 +1,23 @@
+public class PoolPuzzle {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 1 ) {
+            if (x < 1) {
+                System.out.print("a ");
+            }
+            x = x - 1;
+        if (x < 4) {
+            System.out.println("noise");
+            x = x + 2;
+        }
+        if ( x == 1) {
+            System.out.println("annoys");
+        }
+        if (x > 0) {
+            System.out.print("an");
+        }
+            System.out.print("");
+            System.out.println(" oyster");
+        }
+    }
+}
---
+++ tmp/1a/Exersice1a.java
@@ -0,0 +1,12 @@
+class Exersice1a {
+	public static void main (String [] args) {
+		int x = 1;
+		while (x < 10) {
+			x +=1; 
+			if (x > 3) {
+				System.out.println("большой икс");
+			
+			}
+		}
+	}
+}
